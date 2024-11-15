49b4db669a1d05cf4be38bb8812d5dd7b23213ef
Bohdan Sokyrko <bosokyrk@c1r3s1.42wolfsburg.de>
2024-11-15 16:09:38 +0100

    sass

---
+++ ft_strncmp.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    int i;
+
+    i = 0;
+    if (n == 0)
+        return (0);
+    while((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0') && (i < n - 1))
+        i++;
+    return (s1[i] - s2[i]);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     const char s1[] = "amo";
+//     const char s2[] = "amoagmn";
+//     int n = 0;
+//     int out = ft_strncmp(s1, s2, n);
+//     int out2 = strncmp(s1, s2, n);
+//     printf("%d || ", out );
+//     printf("%d", out2);
+// }
